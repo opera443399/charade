820b595ebf2264fcad72d621bccbb60f7618c88c
opera443399 <pc@pcswo.com>
2016-02-17 18:19:59 +0800

    regroup tpl layout for col-xs/sm/md

--- www/static/accounts/js/accounts-base.js
+++ www/static/accounts/js/accounts-base.js
@@ -1,11 +1,7 @@
 /* base */
 $(document).ready(function(){
     $("#quote").hide();
-    $("#help").hide();
     $("#tips1").click(function(){
         $("#quote").slideToggle("slow");
-    });
-    $("#tips2").click(function(){
-        $("#help").slideToggle("slow");
     });
 });
--- www/www/urls.py
+++ www/www/urls.py
@@ -30,5 +30,4 @@ urlpatterns = [
     #################################### admin
     #
     url(r'^admin/', include(admin.site.urls)),
-
 ]
