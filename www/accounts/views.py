d94f921525b126d668f7230311aa40151940ab59
opera443399 <pc@pcswo.com>
2016-02-19 16:58:15 +0800

    simplfy views in accounts; fixed bootstrap layout mistakes;

--- www/accounts/views.py
+++ www/accounts/views.py
@@ -5,9 +5,8 @@
 # @ pc
 ###################################
 
-from django.shortcuts import redirect, resolve_url
+from django.shortcuts import render, redirect, resolve_url
 from django.core.urlresolvers import reverse
-from django.template.response import TemplateResponse
 from django.utils.translation import ugettext as _
 from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
@@ -17,25 +16,15 @@ from .forms import RegistrationForm
 
 # Create your views here.
 
-def registration(request,
-             template_name='accounts/registration.html',
-             registration_form=RegistrationForm,
-             registered_user_redirect_to=None,
-             post_registration_redirect=None,
-             current_app=None,
-             extra_context=None):
-
-    if registered_user_redirect_to is None:
-        registered_user_redirect_to = conf.LOGIN_REDIRECT_URL
+def registration(request, registration_form=RegistrationForm):
+    registered_user_redirect_to = conf.LOGIN_REDIRECT_URL
+    post_registration_redirect = reverse('accounts:registration_finished')
 
     if request.user.is_authenticated():
-            return redirect(registered_user_redirect_to)
+        return redirect(registered_user_redirect_to)
 
     if not conf.REGISTRATION_IS_OPEN:
         return redirect(reverse('accounts:registration_closed'))
-
-    if post_registration_redirect is None:
-        post_registration_redirect = reverse('accounts:registration_finished')
 
     if request.method == 'POST':
         form = registration_form(request.POST)
@@ -48,42 +37,16 @@ def registration(request,
     else:
         form = registration_form()
 
-    current_site = get_current_site(request)
-
     context = {
-        'form': form,
-        'site': current_site,
-        'site_name': current_site.name,
-        'title': _('Register'),
+        'thisform': form,
     }
 
-    if extra_context is not None:
-        context.update(extra_context)
-    return TemplateResponse(request, template_name, context,
-                            current_app=current_app)
-
-def registration_closed(request,
-                        template_name='accounts/registration_closed.html',
-                        current_app=None,
-                        extra_context=None):
-    context = {
-        'title': _('Registration closed'),
-    }
-    if extra_context is not None:
-        context.update(extra_context)
-    return TemplateResponse(request, template_name, context,
-                            current_app=current_app)
+    return render(request, 'accounts/registration.html', context)
 
 
-def registration_finished(request,
-                          template_name='accounts/registration_finished.html',
-                          current_app=None,
-                          extra_context=None):
-    context = {
-        'login_url': resolve_url(conf.LOGIN_URL),
-        'title': _('Registration finished'),
-    }
-    if extra_context is not None:
-        context.update(extra_context)
-    return TemplateResponse(request, template_name, context,
-                            current_app=current_app)
+def registration_closed(request):
+    return render(request, 'accounts/registration_closed.html')
+
+
+def registration_finished(request):
+    return render(request, 'accounts/registration_finished.html')
