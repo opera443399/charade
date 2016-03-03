323fe4c8a58841314e110a3a755048163311b697
opera443399 <pc@pcswo.com>
2016-03-03 17:38:56 +0800

    remove unused import statement

--- www/charade/views.py
+++ www/charade/views.py
@@ -6,13 +6,12 @@
 ###################################
 
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
 from django.utils.translation import ugettext_lazy as _
 import time
 
